001871aa54441018b947e705b8dafc321b71634e
artpro-studio <ya.andreyi96@yandex.ru>
2025-02-26 10:36:58 +0300

    Поправил url для axios

--- admin/src/boot/axios.ts
+++ admin/src/boot/axios.ts
@@ -37,15 +37,16 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({
-    baseURL: 'http://localhost:3000',
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-    },
-});
 
 export default boot(({ app, store }) => {
     // for use inside Vue files (Options API) through this.$axios and this.$api
+
+    const api = axios.create({
+        baseURL: import.meta.env.VITE_APP_BACKEND,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
 
     api.interceptors.request.use(async (configAxios) => {
         configAxios.headers.Authorization = `Bearer ${store.getters['authorizationModule/getToken']}`;
