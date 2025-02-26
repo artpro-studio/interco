c248a65a36c2c59084755b84bee2991c805a7303
artpro-studio <ya.andreyi96@yandex.ru>
2025-02-26 11:35:36 +0300

    axios

--- admin/src/boot/axios.ts
+++ admin/src/boot/axios.ts
@@ -38,16 +38,15 @@ declare module '@vue/runtime-core' {
 // "export default () => {}" function below (which runs individually
 // for each client)
 
-export default boot(({ app, store }) => {
-    // for use inside Vue files (Options API) through this.$axios and this.$api
-    console.log('test', import.meta.env.VITE_APP_BACKEND);
-    const api = axios.create({
-        baseURL: import.meta.env.VITE_APP_BACKEND,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
+ // for use inside Vue files (Options API) through this.$axios and this.$api
+const api = axios.create({
+    baseURL: import.meta.env.VITE_APP_BACKEND,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
+});
 
+export default boot(({ app, store }) => {
     api.interceptors.request.use(async (configAxios) => {
         configAxios.headers.Authorization = `Bearer ${store.getters['authorizationModule/getToken']}`;
         return configAxios;
