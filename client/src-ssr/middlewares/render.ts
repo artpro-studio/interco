5e997d94f3b9a63428574e0634fa0e5a5e3d8f5e
artpro-studio <ya.andreyi96@yandex.ru>
2025-03-04 21:18:19 +0300

    Метрика

--- client/src-ssr/middlewares/render.ts
+++ client/src-ssr/middlewares/render.ts
@@ -15,6 +15,28 @@ export default defineSsrMiddleware(({ app, resolve, render, serve }) => {
     render(/* the ssrContext: */ { req, res })
       .then((html) => {
         // now let's send the rendered html to the client
+		const yandexMetrikaCode = `
+          <script type="text/javascript">
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+              k.async=1;
+              k.src=r;
+              a.parentNode.insertBefore(k,a);
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(100147884, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          </script>
+          <noscript>
+            <div><img src="https://mc.yandex.ru/watch/YOUR_COUNTER_ID" style="position:absolute; left:-9999px;" alt="" /></div>
+          </noscript>
+        `;
+		html = html.replace('</head>', `${yandexMetrikaCode}</head>`);
         res.send(html);
       })
       .catch((err: RenderError) => {
