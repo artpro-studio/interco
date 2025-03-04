1e6fc7a1de4260fdb59af97a9a6231c9c113b7d9
artpro-studio <ya.andreyi96@yandex.ru>
2025-03-04 22:48:26 +0300

    Локализация

--- client/src/boot/i18n.ts
+++ client/src/boot/i18n.ts
@@ -9,7 +9,7 @@ function getUserLocale() {
 		'ru-RU': 'ru-RU',
 		'en-US': 'en-US',
 		'zh-CN': 'zh-CN',
-	}
+	};
 	const locale = navigator.language!.toString(); // 'ru-RU', 'zh-CN', 'en-US'
 	const shortLocale = langs[locale] || 'en-US';
 	return shortLocale; // Если нет в списке, ставим английский
@@ -17,29 +17,47 @@ function getUserLocale() {
 
 export type MessageLanguages = keyof typeof messages;
 // Type-define 'en-US' as the master schema for the resource
-export type MessageSchema = typeof messages['en-US'];
+export type MessageSchema = (typeof messages)['en-US'];
 
 // See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 declare module 'vue-i18n' {
-  // define the locale messages schema
-  export interface DefineLocaleMessage extends MessageSchema {}
+	// define the locale messages schema
+	export interface DefineLocaleMessage extends MessageSchema {}
 
-  // define the datetime format schema
-  export interface DefineDateTimeFormat {}
+	// define the datetime format schema
+	export interface DefineDateTimeFormat {}
 
-  // define the number format schema
-  export interface DefineNumberFormat {}
+	// define the number format schema
+	export interface DefineNumberFormat {}
 }
 /* eslint-enable @typescript-eslint/no-empty-object-type */
 
-export default defineBoot(({ app }) => {
-  const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
-	locale: getUserLocale(),
-    legacy: false,
-    messages,
-  });
+export default defineBoot(({ app, ssrContext }) => {
+	let locale = getUserLocale();
+	if (ssrContext) {
+		const req = ssrContext.req;
+		const host = req.headers.host!;
+		const subdomain = host.split('.')[0]!;
+		let urlLocale = '';
+		if (['en', 'ru', 'ch'].includes(subdomain)) {
+			urlLocale = subdomain;
+		}
+		if (urlLocale && urlLocale.length) {
+			const langsUrl: any = {
+				ru: 'ru-RU',
+				en: 'en-US',
+				ch: 'zh-CN',
+			};
+			locale = langsUrl[urlLocale];
+		}
+	}
+	const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
+		locale: locale,
+		legacy: false,
+		messages,
+	});
 
-  // Set i18n instance on app
-  app.use(i18n);
+	// Set i18n instance on app
+	app.use(i18n);
 });
